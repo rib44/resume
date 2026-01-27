2514c158cf6f0a1beb9a7ea3befd65be8a2c98e7
Aryan Kumar <aryankr730@gmail.com>
2026-01-28 03:45:33 +0530

    added output to print static website url after provision

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # output "static_website_url" {
 #   value = azurerm_storage_account.storage_acc.primary_web_endpoint
 # }
@@ -9,3 +10,8 @@ output "storage_account_name" {
 # output "cosmosdb_account_name" {
 #   value = azurerm_cosmosdb_account.resume-db.name
 # }
+=======
+output "static_website_url" {
+    value = azurerm_storage_account_static_website.front-page.primary_web_endpoint
+}
+>>>>>>> 286418f (added output to print static website url after provision)
