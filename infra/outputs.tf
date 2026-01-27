f4a4c8ed0413029c3388b373bd3a44485574f95e
Aryan Kumar <aryankr730@gmail.com>
2026-01-28 04:02:43 +0530

    modified output.tf to print storage_acc_name

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # output "static_website_url" {
 #   value = azurerm_storage_account.storage_acc.primary_web_endpoint
 # }
@@ -10,8 +9,3 @@ output "storage_account_name" {
 # output "cosmosdb_account_name" {
 #   value = azurerm_cosmosdb_account.resume-db.name
 # }
-=======
-output "static_website_url" {
-    value = azurerm_storage_account_static_website.front-page.primary_web_endpoint
-}
->>>>>>> 286418f (added output to print static website url after provision)
--- infra/storage.tf
+++ infra/storage.tf
@@ -1,6 +1,6 @@
 # Storage Account for Static Website Hosting
 resource "azurerm_storage_account" "storage_acc" {
-  name                     = "${var.storage_account_name}${random_integer.ri.result}"
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.resume-rg.name
   location                 = azurerm_resource_group.resume-rg.location
   account_kind             = "StorageV2"
