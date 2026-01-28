b790459d1e45c6f37f72cc89aabceba21b0d7a58
Aryan Kumar <aryankr730@gmail.com>
2026-01-28 13:19:35 +0530

    added cosmosdb IaC \

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,7 @@
 output "static_website_url" {
-    value = azurerm_storage_account_static_website.front-page.primary_web_endpoint
+  value = azurerm_storage_account.storage_acc.primary_web_endpoint
 }
 
 output "storage_account_name" {
-    value = azurerm_storage_account.storage_acc.name
+  value = azurerm_storage_account.storage_acc.name
 }
