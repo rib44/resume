28fcbeeab4b52e6d1b0374aedc18f5563d858ad2
Aryan Kumar <aryankr730@gmail.com>
2026-01-28 13:19:35 +0530

    added cosmosdb IaC \

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
-# output "static_website_url" {
-#   value = azurerm_storage_account.storage_acc.primary_web_endpoint
-# }
+output "static_website_url" {
+  value = azurerm_storage_account.storage_acc.primary_web_endpoint
+}
 
 output "storage_account_name" {
   value = azurerm_storage_account.storage_acc.name
--- infra/providers.tf
+++ infra/providers.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.8.1"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.8.1"
+    }
   }
 
   required_version = "~> 1.14.3"
