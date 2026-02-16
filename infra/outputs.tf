0f8b1b4fdc8bba7ea945db26732cba1125e7dda3
Aryan Kumar <aryankr730@gmail.com>
2026-02-16 23:11:50 +0530

    commented resources until proper implementation.

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,11 @@
-output "static_website_url" {
-  value = azurerm_storage_account.storage_acc.primary_web_endpoint
-}
+# output "static_website_url" {
+#   value = azurerm_storage_account.storage_acc.primary_web_endpoint
+# }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.storage_acc.name
-}
+# output "storage_account_name" {
+#   value = azurerm_storage_account.storage_acc.name
+# }
+
+# output "cosmosdb_account_name" {
+#   value = azurerm_cosmosdb_account.resume-db.name
+# }
