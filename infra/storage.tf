037c5d3cfd51f11797eaa3c4b6d47e666593f848
Aryan Kumar <aryankr730@gmail.com>
2026-02-16 23:11:50 +0530

    commented resources until proper implementation.

--- infra/storage.tf
+++ infra/storage.tf
@@ -1,17 +1,17 @@
-# Storage Account for Static Website Hosting
-resource "azurerm_storage_account" "storage_acc" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.resume-rg.name
-  location                 = azurerm_resource_group.resume-rg.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
-}
+# # Storage Account for Static Website Hosting
+# resource "azurerm_storage_account" "storage_acc" {
+#   name                     = var.storage_account_name
+#   resource_group_name      = azurerm_resource_group.resume-rg.name
+#   location                 = azurerm_resource_group.resume-rg.location
+#   account_kind             = "StorageV2"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   min_tls_version          = "TLS1_2"
+# }
 
-# Static Website Configuration for Storage Account
-resource "azurerm_storage_account_static_website" "front-page" {
-  storage_account_id = azurerm_storage_account.storage_acc.id
-  index_document     = "index.html"
-  error_404_document = "404.html"
-}
+# # Static Website Configuration for Storage Account
+# resource "azurerm_storage_account_static_website" "front-page" {
+#   storage_account_id = azurerm_storage_account.storage_acc.id
+#   index_document     = "index.html"
+#   error_404_document = "404.html"
+# }
