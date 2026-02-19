315f84d014a1a2e1201b3ec2c794a39adb4d9665
Aryan Kumar <aryankr730@gmail.com>
2026-02-20 01:28:24 +0530

    added sessionTracker class to manage browser storage uses sessionStorage

--- app_frontend/scripts/script.js
+++ app_frontend/scripts/script.js
@@ -14,5 +14,20 @@ class ApiClient {
     }
 }
 
+// Handles browser storage
+class SessionTracker {
+    constructor(storagekey = "hasVisited") {
+        this.storagekey = storagekey;
+    }
+
+    hasVisited() {
+        return sessionStorage.getItem(this.storagekey) !== null;
+    }
+
+    markAsVisited() {
+        sessionStorage.setItem(this.storagekey, "true");
+    }
+}
 
 const apiClient = new ApiClient();
+const sessionTracker = new SessionTracker("resume_visited");
