733c19a7abe3c217e18817b3137a351242bb1a32
Aryan Kumar <aryankr730@gmail.com>
2026-02-20 03:19:05 +0530

    bugfix: saving visitorCount to sessionStorage

--- app_frontend/scripts/script.js
+++ app_frontend/scripts/script.js
@@ -16,16 +16,25 @@ class ApiClient {
 
 // Handles browser storage
 class SessionTracker {
-    constructor(storagekey = "hasVisited") {
-        this.storagekey = storagekey;
+    constructor(visitedKey = "resume_visited", countKey="resume_count") {
+        this.visitedKey = visitedKey;
+        this.countKey = countKey;
     }
 
     hasVisited() {
-        return sessionStorage.getItem(this.storagekey) !== null;
+        return sessionStorage.getItem(this.visitedKey) !== null;
     }
 
     markAsVisited() {
-        sessionStorage.setItem(this.storagekey, "true");
+        sessionStorage.setItem(this.visitedKey, "true");
+    }
+
+    saveCount(count) {
+        sessionStorage.setItem(this.countKey, count.toString());
+    }
+
+    getSavedCount() {
+        sessionStorage.getItem(this.countKey);
     }
 }
 
@@ -58,14 +67,24 @@ class VisitorController {
     async processVisit() {
         // prevents refresh spam
         if (this.sessionTracker.hasVisited()) {
-            console.log("Same Visitor");
-            return;
+            console.log("Same Visitor, using cached count");
+
+            const cachedCount = this.sessionTracker.getSavedCount();
+            if (cachedCount) {
+                this.counterUI.updateCount(cachedCount);
+            }
+            return; // No API Call
         }
 
+        // API call: first-time user
         try {
             const data = await this.apiClient.post(this.apiUrl);
+
             this.counterUI.updateCount(data.visitors);
+
             this.sessionTracker.markAsVisited();
+            this.sessionTracker.saveCount(data.visitors);
+
         } catch (error) {
             console.error("Error updating visitor count: ", error);
             this.counterUI.showError();
@@ -73,6 +92,7 @@ class VisitorController {
     }
 }
 
+// -------------------Application bootstap here-----------------
 const apiClient = new ApiClient();
 const sessionTracker = new SessionTracker("resume_visited");
 const counterUI = new CounterUI("cnt")
@@ -81,6 +101,7 @@ const apiUrl = `${config.apiBaseUrl}/visitor`;
 // inject services into the main orchestrator
 const visitorCounter = new VisitorController(apiClient, sessionTracker, counterUI, apiUrl);
 
+// call the API after 2s, prevents crawler-bot API call
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         visitorCounter.processVisit();
